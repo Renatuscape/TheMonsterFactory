beb387a91ec1f520bd0f7a0694aa0f1816633116
Renatuscape <silarcta@gmail.com>
2023-11-20 14:49:49 +0100

    Added static name generator for people and monsters.

---
+++ MonsterFactory/DataAccess/NameGenerator.cs
@@ -0,0 +1,28 @@
+﻿namespace TheMonsterFactory.DataAccess
+{
+    public class NameGenerator
+    {
+        public Random random = new Random();
+        static List<string> PeopleNames { get; } = new()
+        {
+            "John","Jim", "Finn", "Carl", "Raymond", "Anna", "Michael", "Joanne", "Morag", "Charlie"
+        };
+
+        static List<string> MonsterNames { get; } = new()
+        {
+            "Grok", "Murmank", "Fronk", "Bralg", "Jort", "Smerdok", "Churl", "Arbadonk", "Urloz", "Wurtapek"
+        };
+
+        public string GetRandomName(bool isPerson = false)
+        {
+            if (isPerson)
+            {
+                return PeopleNames[random.Next(PeopleNames.Count)];
+            }
+            else
+            {
+                return MonsterNames[random.Next(MonsterNames.Count)];
+            }
+        }
+    }
+}
