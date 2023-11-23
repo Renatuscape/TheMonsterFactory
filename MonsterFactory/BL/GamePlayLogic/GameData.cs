90370f29e2918ee729963d48681d427fc3c97156
Renatuscape <silarcta@gmail.com>
2023-11-23 19:10:13 +0100

    Added GameData class to hold information that needs to be passed through every method.

---
+++ MonsterFactory/BL/GamePlayLogic/GameData.cs
@@ -0,0 +1,20 @@
+﻿using TheMonsterFactory.BL.Heroes;
+using TheMonsterFactory.BL.Monsters;
+
+namespace TheMonsterFactory.BL.GamePlay
+{
+    public class GameData
+    {
+        public ITextManagement TextManager { get; set; }
+        public List<Hero> HeroList { get; set; }
+        public List<Monster> MonsterList { get; set; }
+        public int MonsterLevel { get; set; } = 1;
+        public int PlayerLevel { get; set; } = 1;
+        public int GameRound { get; set; } = 0;
+        public GameData(ITextManagement textManager) { 
+            TextManager = textManager;
+            HeroList = new List<Hero>();
+            MonsterList = new List<Monster>();
+        }
+    }
+}
