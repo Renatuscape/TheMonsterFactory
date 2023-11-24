96f984f00f4834b45e3945e836eba04c41b0fd95
Renatuscape <silarcta@gmail.com>
2023-11-24 17:50:51 +0100

    added missing while loop, ensuring that the player must choose a valid target before proceeding.

--- MonsterFactory/BL/GamePlayLogic/Targeting.cs
+++ MonsterFactory/BL/GamePlayLogic/Targeting.cs
@@ -8,48 +8,22 @@ namespace TheMonsterFactory.BL.GamePlay
         public static int ChooseAlly(GameData gameData)
         {
             int target = -1;
-            for (int i = 0; i < gameData.HeroList.Count; i++)
+            while (target <= -1 || target >= gameData.HeroList.Count)
             {
-                gameData.TextManager.WriteLine($"{i}: {gameData.HeroList[i].ShortStats()}");
-            }
-
-            string input = gameData.TextManager.ReadKey();
-
-            if (int.TryParse(input, out var choice))
-            {
-                target = choice;
-            }
-
-            if (target >= gameData.HeroList.Count || target < 0)
-            {
-                target = -1;
-            }
-
-            if (target <= -1)
-            {
-                gameData.TextManager.WriteLine($"Please choose a valid target.");
-                gameData.TextManager.ContinueAfterAnyKey();
-            }
-            return target;
-        }
-        public static int ChooseEnemy(GameData gameData)
-        {
-            int target = -1;
-
-            if (gameData.MonsterList != null)
-            {
-                for (int i = 0; i < gameData.MonsterList.Count; i++)
+                for (int i = 0; i < gameData.HeroList.Count; i++)
                 {
-                    gameData.TextManager.WriteLine($"{i}: {gameData.MonsterList[i].ShortStats()}");
+                    gameData.TextManager.WriteLine($"{i}: {gameData.HeroList[i].ShortStats()}");
                 }
 
                 string input = gameData.TextManager.ReadKey();
+                gameData.TextManager.WriteLine("");
 
                 if (int.TryParse(input, out var choice))
                 {
                     target = choice;
                 }
-                if (target >= gameData.MonsterList.Count || target < 0)
+
+                if (target >= gameData.HeroList.Count || target < 0)
                 {
                     target = -1;
                 }
@@ -58,6 +32,40 @@ namespace TheMonsterFactory.BL.GamePlay
                 {
                     gameData.TextManager.WriteLine($"Please choose a valid target.");
                     gameData.TextManager.ContinueAfterAnyKey();
+                }
+            }
+            return target;
+        }
+        public static int ChooseEnemy(GameData gameData)
+        {
+            int target = -1;
+
+            while (target <= -1 || target >= gameData.HeroList.Count)
+            {
+                if (gameData.MonsterList != null)
+                {
+                    for (int i = 0; i < gameData.MonsterList.Count; i++)
+                    {
+                        gameData.TextManager.WriteLine($"{i}: {gameData.MonsterList[i].ShortStats()}");
+                    }
+
+                    string input = gameData.TextManager.ReadKey();
+                    gameData.TextManager.WriteLine("");
+
+                    if (int.TryParse(input, out var choice))
+                    {
+                        target = choice;
+                    }
+                    if (target >= gameData.MonsterList.Count || target < 0)
+                    {
+                        target = -1;
+                    }
+
+                    if (target <= -1)
+                    {
+                        gameData.TextManager.WriteLine($"Please choose a valid target.");
+                        gameData.TextManager.ContinueAfterAnyKey();
+                    }
                 }
             }
             return target;
