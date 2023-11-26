9789e3f7bdb15cdce31ded1209b1e9f8719a2d84
Renatuscape <rrjonsberg@gmail.com>
2023-11-26 12:53:58 +0100

    Gold counter added to GameData. Calls Moves to initialise list of all moves when the game starts.

--- MonsterFactory/BL/GamePlayLogic/GameData.cs
+++ MonsterFactory/BL/GamePlayLogic/GameData.cs
@@ -1,4 +1,5 @@
-﻿using TheMonsterFactory.BL.Heroes;
+﻿using TheMonsterFactory.BL.CombatMoves;
+using TheMonsterFactory.BL.Heroes;
 using TheMonsterFactory.BL.Monsters;
 
 namespace TheMonsterFactory.BL.GamePlay
@@ -11,11 +12,13 @@ namespace TheMonsterFactory.BL.GamePlay
         public List<Monster> MonsterList { get; set; }
         public int MonsterLevel { get; set; } = 1;
         public int PlayerLevel { get; set; } = 1;
+        public int Gold { get; set; } = 110;
         public int GameRound { get; set; } = 0;
         public GameData(ITextManagement textManager) { 
             TextManager = textManager;
             HeroList = new List<Hero>();
             MonsterList = new List<Monster>();
+            Moves.PopulateList();
         }
     }
 }
