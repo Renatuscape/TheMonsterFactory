5bf825ed120f80daadabae053059c5192df3f756
Renatuscape <rrjonsberg@gmail.com>
2023-11-26 13:46:39 +0100

    Hero is now abstract, for hero classes to derive from.

--- MonsterFactory/BL/Heroes/Hero.cs
+++ MonsterFactory/BL/Heroes/Hero.cs
@@ -2,19 +2,11 @@
 
 namespace TheMonsterFactory.BL.Heroes
 {
-    public class Hero : Creature, IAttack
+    public abstract class Hero : Creature
     {
         public Hero(string name, int level) : base(name, level)
         {
-            Description = "A simple adventurer.";
-            Moves.Find("Iron Sword", MoveList);
-        }
 
-        public virtual int Attack(out string description)
-        {
-            int amount = Die.Roll(Level) * 2;
-            description = $"{Name} swings their sword for {amount} damage!";
-            return amount;
         }
     }
 }
