a05e5ca8160f5d6da9da5949f327a8aa7531aa18
Renatuscape <rrjonsberg@gmail.com>
2023-11-26 13:46:52 +0100

    New Fighter class added to take over old Hero role.

---
+++ MonsterFactory/BL/Heroes/Fighter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMonsterFactory.BL.CombatMoves;
+
+namespace TheMonsterFactory.BL.Heroes
+{
+    public class Fighter : Hero
+    {
+        public Fighter(string name, int level) : base(name, level)
+        {
+            BaseCost = 15;
+
+            Description = "A brave, bright-eyed adventurer.";
+            Moves.Find("Iron Sword", MoveList);
+        }
+    }
+}
