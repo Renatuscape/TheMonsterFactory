5b84a4424da5346f7876b016ccf50c13d4fc427a
Renatuscape <rrjonsberg@gmail.com>
2023-11-26 22:56:06 +0100

    Added correct move to goblin class.

--- MonsterFactory/BL/Monsters/Goblin.cs
+++ MonsterFactory/BL/Monsters/Goblin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheMonsterFactory.BL.CombatMoves;
 
 namespace TheMonsterFactory.BL.Monsters
 {
@@ -10,7 +11,7 @@ namespace TheMonsterFactory.BL.Monsters
     {
         public Goblin(string name, int level) : base(name, level)
         {
-
+            Moves.Find("Claws", MoveList);
         }
 
         public override int Attack(out string description)
