842eeac98f2bbe8932ef4b59c9308d522b9a81fb
Renatuscape <rrjonsberg@gmail.com>
2023-12-03 12:40:32 +0100

    Fixed bug where player level was 0 instead of current chapter as starting values

--- MonsterFactory/BL/GamePlayLogic/GameData.cs
+++ MonsterFactory/BL/GamePlayLogic/GameData.cs
@@ -11,8 +11,8 @@ namespace TheMonsterFactory.BL.GamePlay
         public List<Hero> HeroList { get; set; }
         public List<Monster> MonsterList { get; set; }
         public int MonsterLevel { get; set; } = 1;
-        public int PlayerLevel { get; set; } = 0;
-        public int CurrentChapter { get; set; } = 1;
+        public int PlayerLevel { get; set; } = 1;
+        public int CurrentChapter { get; set; } = 0;
         public int Gold { get; set; } = 65;
         public int GameRound { get; set; } = 0;
         public GameData(ITextManagement textManager)
