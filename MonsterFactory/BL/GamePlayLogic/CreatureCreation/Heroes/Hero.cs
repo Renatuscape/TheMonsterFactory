f8cfc9ffa3e01b0284b84b574ef4185e2c010d69
Renatuscape <silarcta@gmail.com>
2023-12-07 11:49:52 +0100

    added variable for AdvancedLevel when the unit can be promoted.

--- MonsterFactory/BL/GamePlayLogic/CreatureCreation/Heroes/Hero.cs
+++ MonsterFactory/BL/GamePlayLogic/CreatureCreation/Heroes/Hero.cs
@@ -5,6 +5,7 @@ namespace TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Heroes
 {
     public abstract class Hero : Creature
     {
+        public int AdvanceLevel { get; set; } = 3;
         public Hero(string name, int level) : base(name, level)
         {
 
