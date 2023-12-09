dda771bc358afc59581922798529be8ffc473492
Renatuscape <silarcta@gmail.com>
2023-12-09 10:32:29 +0100

    Added IRender object to gameData.

--- MonsterFactory/BL/GamePlayLogic/GameData.cs
+++ MonsterFactory/BL/GamePlayLogic/GameData.cs
@@ -1,6 +1,7 @@
 ﻿using TheMonsterFactory.BL.CombatMoves;
 using TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Heroes;
 using TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Monsters;
+using TheMonsterFactory.Rendering;
 
 namespace TheMonsterFactory.BL.GamePlay
 {
@@ -8,6 +9,7 @@ namespace TheMonsterFactory.BL.GamePlay
     {
         public Random randomiser = new Random();
         public ITextManagement TextManager { get; set; }
+        public IRender Render {  get; set; }
         public List<Hero> HeroList { get; set; }
         public List<Monster> MonsterList { get; set; }
         public int MonsterLevel { get; set; } = 1;
@@ -15,9 +17,10 @@ namespace TheMonsterFactory.BL.GamePlay
         public int CurrentChapter { get; set; } = 0;
         public int Gold { get; set; } = 65;
         public int GameRound { get; set; } = 0;
-        public GameData(ITextManagement textManager)
+        public GameData(ITextManagement textManager, IRender render)
         { 
             TextManager = textManager;
+            Render = render;
             HeroList = new List<Hero>();
             MonsterList = new List<Monster>();
             Moves.PopulateList();
