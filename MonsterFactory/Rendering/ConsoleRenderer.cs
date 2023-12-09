03ec20c1a759d1cb1750ad7acae05c6d9de3bfe2
Renatuscape <silarcta@gmail.com>
2023-12-09 10:33:32 +0100

    Created rendering interface and console rendering class

---
+++ MonsterFactory/Rendering/ConsoleRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMonsterFactory.BL.GamePlay;
+using TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Heroes;
+using TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Monsters;
+
+namespace TheMonsterFactory.Rendering
+{
+    public class ConsoleRenderer : IRender
+    {
+        public void HeroRoster(GameData gameData)
+        {
+            string heroString = string.Empty;
+            foreach (Hero hero in gameData.HeroList)
+            {
+                heroString += $"|{hero} {hero.CurrentHealth}/{hero.MaxHealth} ";
+            }
+            gameData.TextManager.WriteLine(heroString + "\n");
+        }
+
+        public void MonsterRoster(GameData gameData)
+        {
+            string monsterString = string.Empty;
+            foreach (Monster monster in gameData.MonsterList)
+            {
+                monsterString += $"|{monster} {monster.CurrentHealth}/{monster.MaxHealth} ";
+            }
+            gameData.TextManager.WriteLine(monsterString + "\n");
+        }
+
+        public void Roster(GameData gameData)
+        {
+            string heroString = string.Empty;
+            string monsterString = string.Empty;
+            foreach (Hero hero in gameData.HeroList)
+            {
+                heroString += $"|{hero} {hero.CurrentHealth}/{hero.MaxHealth} ";
+            }
+            foreach (Monster monster in gameData.MonsterList)
+            {
+                monsterString += $"|{monster} {monster.CurrentHealth}/{monster.MaxHealth} ";
+            }
+            gameData.TextManager.WriteLine(heroString + "\n" + monsterString + "\n");
+        }
+    }
+}
---
+++ MonsterFactory/Rendering/IRender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMonsterFactory.BL.GamePlay;
+
+namespace TheMonsterFactory.Rendering
+{
+    public interface IRender
+    {
+        public void Roster(GameData gameData);
+
+        public void HeroRoster(GameData gameData);
+
+        public void MonsterRoster(GameData gameData);
+    }
+}
