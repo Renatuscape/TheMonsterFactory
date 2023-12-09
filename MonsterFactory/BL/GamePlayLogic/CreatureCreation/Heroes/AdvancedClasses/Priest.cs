cc51d863cd0805fb00b5506d5e187b1291f40152
Renatuscape <silarcta@gmail.com>
2023-12-09 10:26:01 +0100

    Created new advanced hero class

---
+++ MonsterFactory/BL/GamePlayLogic/CreatureCreation/Heroes/AdvancedClasses/Priest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMonsterFactory.BL.CombatMoves;
+using TheMonsterFactory.BL.DiceLogic;
+
+namespace TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Heroes.AdvancedClasses
+{
+    public class Priest : AdvancedHero
+    {
+        public Priest(string name, int level) : base(name, level)
+        {
+            Evasiveness = 0;
+            Description = "A versatile healer.";
+            HealthDie = new D6();
+            Moves.Find("Staff", MoveList);
+            Moves.Find("Cure", MoveList);
+            Moves.Find("Cure All", MoveList);
+            Moves.Find("Ward", MoveList);
+        }
+    }
+}
