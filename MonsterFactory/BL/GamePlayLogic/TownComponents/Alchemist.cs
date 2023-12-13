feef6cad4c4104a5fc0fc9938b98b8f737783863
Renatuscape <silarcta@gmail.com>
2023-12-13 18:08:55 +0100

    Prepared method for max health increase

--- MonsterFactory/BL/GamePlayLogic/TownComponents/Alchemist.cs
+++ MonsterFactory/BL/GamePlayLogic/TownComponents/Alchemist.cs
@@ -1,5 +1,6 @@
 ﻿using MonsterFactory.UI;
 using TheMonsterFactory.BL.GamePlay;
+using TheMonsterFactory.BL.GamePlayLogic.CreatureCreation.Heroes;
 
 namespace TheMonsterFactory.BL.GamePlayLogic.ShopComponents
 {
@@ -7,8 +8,14 @@ namespace TheMonsterFactory.BL.GamePlayLogic.ShopComponents
     {
         public static void VisitAlchemist(GameData gameData)
         {
-            gameData.TextManager.WriteColour($"The alchemist will sell useful items and upgrades.", ColourTag.Default);
+            gameData.TextManager.WriteColour($"The alchemist sells useful items and upgrades.", ColourTag.Default);
             gameData.TextManager.ContinueAfterAnyKey();
+        }
+
+        static void IncreaseMaxHealth(Hero hero)
+        {
+            hero.MaxHealth++;
+            hero.CurrentHealth = hero.MaxHealth;
         }
     }
 }
