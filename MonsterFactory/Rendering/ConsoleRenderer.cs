085d925ecd7cc20b3187b7dfa9a721be9a2c230c
Renatuscape <silarcta@gmail.com>
2023-12-25 21:54:17 +0100

    Created render methods for indexed lists to replace generic for loops.

--- MonsterFactory/Rendering/ConsoleRenderer.cs
+++ MonsterFactory/Rendering/ConsoleRenderer.cs
@@ -21,6 +21,30 @@ namespace TheMonsterFactory.Rendering
             gameData.TextManager.WriteLine(heroString + "\n");
         }
 
+        public void IndexedHeroList(GameData gameData)
+        {
+            string heroString = string.Empty;
+            int i = 0;
+            foreach (Hero hero in gameData.HeroList)
+            {
+                heroString += $"[{i}] {hero.ShortStats()}\n";
+                i++;
+            }
+            gameData.TextManager.WriteColour(heroString + "\n", MonsterFactory.UI.ColourTag.Information);
+        }
+
+        public void IndexedMonsterList(GameData gameData)
+        {
+            string monsterString = string.Empty;
+            int i = 0;
+            foreach (Monster monster in gameData.MonsterList)
+            {
+                monsterString += $"[{i}] {monster.ShortStats()}\n";
+                i++;
+            }
+            gameData.TextManager.WriteColour(monsterString + "\n", MonsterFactory.UI.ColourTag.Information);
+        }
+
         public void MonsterRoster(GameData gameData)
         {
             string monsterString = string.Empty;
--- MonsterFactory/Rendering/IRender.cs
+++ MonsterFactory/Rendering/IRender.cs
@@ -10,9 +10,9 @@ namespace TheMonsterFactory.Rendering
     public interface IRender
     {
         public void Roster(GameData gameData);
-
         public void HeroRoster(GameData gameData);
-
+        public void IndexedHeroList(GameData gameData);
         public void MonsterRoster(GameData gameData);
+        public void IndexedMonsterList(GameData gameData);
     }
 }
